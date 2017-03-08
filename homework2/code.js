7d21fdd165521d3a687ec396fd48429ec2d9871d
akpersad <akpersad@gmail.com>
2017-03-08 00:52:19 -0500

    Added Nav Bar to Pages

--- homework2/code.js
+++ homework2/code.js
@@ -2,7 +2,14 @@ $('#touch').click(function() {
     $('#images').html("");
     var flickerTag = prompt("Please enter Tag");
     var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    $('#header').html(flickerTag);
+    $('#header').html(flickerTag).css({
+        "text-align": "center",
+        "border": "1px solid black",
+        "padding-top": "10px",
+        "padding-bottom": "10px",
+        "margin-bottom": "20px",
+        "margin-top": "20px"
+    });
     $.getJSON( flickerAPI, {
         tags: flickerTag,
         tagmode: "any",
